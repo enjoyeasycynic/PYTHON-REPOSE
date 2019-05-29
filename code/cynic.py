a2ea237106f66b73c81bb74ba27e7bad182c3ea0
Cynic_oTATo <49015858+enjoyeasy1993@users.noreply.github.com>
2019-05-29 16:36:46 +0800

    Add files via upload

---
+++ code/cynic.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+a = tf.constant(2,name="input_a")
+b = tf.constant(4,name="input_b")
+c = tf.multiply(a,b,name="mul_c")
+d = tf.add(a,b,name="add_d")
+e = tf.add(c,d,name="add_e")
+sess = tf.Session()
+output = sess.run(e)
+print(output)
+writer = tf.summary.FileWriter('home/feigu/tmp',sess.graph)
+writer.close()
+sess.close()
